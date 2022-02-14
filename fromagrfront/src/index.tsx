6e78d15a7c851d95be5f3b403d1609a9adc8f225
nicolas.matet <nicolas.matet@algonomia.com>
2022-02-14 10:38:51 +0100

    firebase start

--- fromagrfront/src/index.tsx
+++ fromagrfront/src/index.tsx
@@ -23,6 +23,27 @@ import "../public/favicon/android-icon-192x192.png"
 import "../public/favicon/favicon-32x32.png"
 import "../public/favicon/favicon-96x96.png"
 import "../public/favicon/favicon-16x16.png"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB3jlAsFChq-fj2vpVxYdr5j6rO4v-aSy0",
+  authDomain: "fromagr-67c78.firebaseapp.com",
+  projectId: "fromagr-67c78",
+  storageBucket: "fromagr-67c78.appspot.com",
+  messagingSenderId: "927232993623",
+  appId: "1:927232993623:web:c45976051c4ec8e6fcf612",
+  measurementId: "G-HBXQBXWKPT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <BrowserRouter>
