f7a80d8b72633b12b1c8debef6c60f03070bc2c4
nicolas.matet <nicolas.matet@algonomia.com>
2022-02-15 15:55:28 +0100

    animation of go back button

--- fromagrfront/src/components/TopBar.tsx
+++ fromagrfront/src/components/TopBar.tsx
@@ -1,4 +1,4 @@
-import { Fab, useTheme } from "@mui/material";
+import { Fab, useTheme, Zoom } from "@mui/material";
 import * as React from 'react';
 import { ColorModeContext } from "../App";
 import Brightness4Icon from '@mui/icons-material/Brightness4';
@@ -8,23 +8,37 @@ import { useNavigate } from "react-router-dom";
 
 
 export function TopBar(props: { goBack?: boolean }) {
-  const { goBack } = props
   const theme = useTheme();
+  const transitionDuration = {
+    enter: theme.transitions.duration.enteringScreen,
+    exit: theme.transitions.duration.leavingScreen,
+  };
+  const { goBack } = props
   const navigate = useNavigate();
   const colorMode = React.useContext(ColorModeContext);
   const content = [
-    <Fab sx={{ position: 'fixed', top: 0, right: 0, m: 2, zIndex:9999 }}
+
+    <Fab sx={{ position: 'fixed', top: 0, right: 0, m: 2, zIndex: 9999 }}
       onClick={colorMode.toggleColorMode}
       size='small' color="secondary" aria-label="theme">
       {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
     </Fab >,
-    ...(goBack ? [
-      <Fab sx={{ position: 'fixed', top: 0, left: 0, m: 2, zIndex:9999 }}
-        onClick={() => { navigate(-1) }}
-        size='small' color="secondary" aria-label="back">
-        <ArrowBackIcon />
-      </Fab>
-    ] : []),
+
+      <Zoom
+        key={'back'}
+        in={goBack === true}
+        timeout={transitionDuration}
+        style={{
+          transitionDelay: `${transitionDuration.exit}ms`,
+        }}
+        unmountOnExit
+      >
+        <Fab sx={{ position: 'fixed', top: 0, left: 0, m: 2, zIndex: 9999 }}
+          onClick={() => { navigate(-1) }}
+          size='small' color="secondary" aria-label="back">
+          <ArrowBackIcon />
+        </Fab>
+      </Zoom>,
   ]
   return (
     <>
